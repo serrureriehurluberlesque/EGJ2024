65beac108473c60b1e2a522e29c935894a77c7d1
Hugo Dupraz <hugo.dupraz@gmail.com>
2024-06-30 11:47:47 +0200

    bug wtf sur lvl 0

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -2,7 +2,8 @@ extends Sprite2D
 
 var nexted = false
 
-func _process(delta):
+func _process(delta): 
 	if not nexted and Input.is_action_pressed("next"):
+		print(nexted)
 		nexted = true
 		get_node("/root/Main").fast_next_level()
