25600d7a1425187951dafe9dd7c8c418bbea81e9
Hugo Dupraz <hugo.dupraz@gmail.com>
2024-06-30 13:55:15 +0200

    fixes sprites title

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -9,8 +9,8 @@ var t = 0
 var sprites = []
 
 func _ready():
-	for i in 2:
-		var p = "res://assets/titlescreen_" + str(i + 2) + ".png"
+	for i in 4:
+		var p = "res://assets/titlescreen_" + str(i) + ".png"
 		if ResourceLoader.exists(p):
 			sprites.append(load(p))
 
@@ -18,7 +18,10 @@ func _process(delta):
 	t += delta
 	if t > 1.0:
 		if togglage:
-			texture = sprites[int(t * 1) % 2]
+			var delt = 0
+			if not loop:
+				delt = 2
+			texture = sprites[(int(t * 1) % 2) + delt]
 
 		if not nexted and Input.is_action_pressed("next"):
 			nexted = true
